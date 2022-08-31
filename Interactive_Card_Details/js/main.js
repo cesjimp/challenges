229a0cf6eb2b3b4a1a43ce91d967828681a63d4d
Cesar Jimenez <cesjimp@gmail.com>
2022-08-31 16:02:32 -0500

    Se inicia archivo JS y se ajustan textos en imagenes tarjetas

--- Interactive_Card_Details/js/main.js
+++ Interactive_Card_Details/js/main.js
@@ -1,1 +1,20 @@
-console.log("probando js")
+const cardHolderNameInput=document.getElementById("nombre")
+const cardNumberInput=document.getElementById("cardnumber")
+const monthExpireInput=document.getElementById("month")
+const yearExpireInput=document.getElementById("year")
+const cvcCodeInput=document.getElementById("cvc")
+const confirmButton=document.getElementById("confirmButton")
+
+
+let cardHolderName=cardHolderNameInput.value;
+let cardNumber=cardNumberInput.value;
+let monthExpire=monthExpireInput.value;
+let yearExpire=yearExpireInput.value;
+let cvcCode=cvcCodeInput.value;
+
+
+
+
+
+console.log({cardHolderNameInput})
+console.log({cardHolderName})
